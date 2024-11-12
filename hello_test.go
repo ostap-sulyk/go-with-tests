4a8d7ce322ef7ac31b88ab0d9cd13bff6b96122e
Ostap Sulyk <ostapsulyk@gmail.com>
2024-11-11 19:59:30 -0500

    learned subtests and helper methods

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,29 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	name := "Chris"
+	t.Run("Say hello to people", func(t *testing.T) {
+		got := Hello(name)
+		want := englishHelloPrefix + name
 
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+	})
+	t.Run("say 'Hello World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
+
+func assertCorrectMessege(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got: %q want %q", got, want)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
